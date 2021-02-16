49f358ab213503ab6d1ce195e461dd439267ba67
Serkan <serkara2de@hotmail.com>
2021-02-16 13:31:11 +0100

    Stepdefs paths are corrected

--- src/test/java/com/CRMLY/runners/CukesRunner.java
+++ src/test/java/com/CRMLY/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports",
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
-        glue = "com/CRMLY/step_definitions",
+        glue = "com/CRMLY/stepdefinitions",
         dryRun = false,
         tags = "@wip"
 )
