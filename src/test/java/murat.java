33942a11cb4934b294dbf070fdab3498eeee16e5
Serkan <serkara2de@hotmail.com>
2021-02-16 11:00:50 +0100

    Practicing files are deleted

--- src/test/java/murat.java
+++
@@ -1,7 +0,0 @@
-public class murat {
-    public static void main(String[] args) {
-        System.out.println("this is from branch murat, hallo Welt!");
-        System.out.println("murat is happy to be in group-17");
-        System.out.println("now, i will merge it!!!");
-    }
-}
